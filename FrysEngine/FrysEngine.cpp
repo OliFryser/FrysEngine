f838a8f216c51357489d2ed03cb5ca5eb3234b8e
OliFryser <oliver-1999@live.dk>
2025-01-04 13:25:27 +0100

    initial commit

---
+++ FrysEngine/FrysEngine.cpp
@@ -0,0 +1,12 @@
+﻿// FrysEngine.cpp : Defines the entry point for the application.
+//
+
+#include "FrysEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
